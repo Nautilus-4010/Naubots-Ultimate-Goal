99344834a949f6cf903d056a8abd69fe8d651b48
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-06-29 21:05:41 -0500

    Create NaubotsAutonomoTres.java

---
+++ NaubotsAutonomoTres.java
@@ -0,0 +1,90 @@
+/*
+Copyright 2019 FIRST Tech Challenge Team 4010
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Stones Blue Alliance", group="Linear Opmode")
+
+public class NaubotsAutonomoTres extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private LaBarca naubot = new LaBarca(this);
+
+    @Override
+    public void runOpMode() {
+
+        naubot.getHardware(hardwareMap);
+        naubot.resetEncoders();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        //Primer Stone
+        naubot.moverDistanciaRecta(60);
+        runtime.reset();
+        double periodo = runtime.milliseconds() + 2000;
+        while(!naubot.boton.isPressed() && runtime.milliseconds() < periodo && opModeIsActive()) {
+            naubot.activarIntake(-1);
+            naubot.leftDrive.setPower(0.3);
+            naubot.rightDrive.setPower(0.3);
+        }
+        naubot.moverDistanciaRecta(-70);
+        naubot.girarEnEje(50);
+        naubot.moverDistanciaRecta(165);
+        naubot.activarIntake(1);
+        sleep(1000);
+        naubot.moverDistanciaRecta(-250);
+        naubot.girarEnEje(-50);
+
+        //Comenzar por el segundo Stone
+        naubot.moverDistanciaRecta(30);
+        periodo = runtime.milliseconds() + 2000;
+        while(!naubot.boton.isPressed() && runtime.milliseconds() < periodo && opModeIsActive()) {
+            naubot.activarIntake(-1);
+            naubot.leftDrive.setPower(0.3);
+            naubot.rightDrive.setPower(0.3);
+        }
+        naubot.moverDistanciaRecta(-90);
+        naubot.girarEnEje(50);
+        naubot.moverDistanciaRecta(190);
+        naubot.activarIntake(1);
+        sleep(1000);
+        naubot.moverDistanciaRecta(-230);
+        naubot.girarEnEje(-45);
+
+        //Comenzar por el tercer Stone
+        naubot.moverDistanciaRecta(60);
+        periodo = runtime.milliseconds() + 2000;
+        while(!naubot.boton.isPressed() && runtime.milliseconds() < periodo && opModeIsActive()) {
+            naubot.activarIntake(-1);
+            naubot.leftDrive.setPower(0.3);
+            naubot.rightDrive.setPower(0.3);
+        }
+        naubot.moverDistanciaRecta(-70);
+        naubot.girarEnEje(50);
+        naubot.moverDistanciaRecta(180);
+        naubot.activarIntake(1);
+        sleep(1000);
+    }
+}
